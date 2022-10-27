afcd8605fe5876c298dd2cfefe7c41792d821d4b
Rozalyn <96503223+ZolaraCodes@users.noreply.github.com>
2022-10-27 06:45:25 -0400

    Time: 111 ms (81.06%), Space: 46.2 MB (38.13%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,9 @@
+const canConstruct = (ransomNote, magazine) => {
+  let dicArr = [...magazine];
+  for (const c of ransomNote) {
+    const index = dicArr.indexOf(c);
+    if (index < 0) return false;
+    dicArr.splice(index, 1);
+  }
+  return true;
+};
